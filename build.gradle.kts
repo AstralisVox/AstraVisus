ad7b74fbb85fd002e64f1e0b4ad5ad0fb22c08e3
OmegaWeaponDev <OmegaWeaponDev@outlook.com.au>
2026-03-04 20:23:04 +1030

    Initial setup of build.gradle.kts and file structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,39 @@
 plugins {
     id("java")
+    id("maven-publish")
+    id("java-library")
+    id("com.gradleup.shadow") version "9.3.2"
 }
 
-group = "me.omegaweapondev"
-version = "1.0-SNAPSHOT"
+group = "me.astravisvox"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.bstats:bstats-bukkit:3.2.1")
+    compileOnly("org.spigotmc:spigot-api:1.21.11-R0.2-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.12.2")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(22))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = Charsets.UTF_8.name()
+    options.release.set(22)
+}
+
+tasks."named("shadowJar", com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar) {
+archiveFileName.set("${project.name}-${project.version}.jar")
+enableRelocation = true
+relocationPrefix = "me.astralisvox.astravisus.libs"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "AstralisVisus"
+rootProject.name = "AstraVisus"
 
